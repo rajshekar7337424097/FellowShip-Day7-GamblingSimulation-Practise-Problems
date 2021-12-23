46186fa2508a3390d4eff7541987d79a92b414ea
rajashekar reddy <chrajashekarreddy777@gmail.com>
2021-12-23 16:08:02 +0530

    adding Gambling uc 1

---
+++ src/com/bl/Gambling/Simulation/GamblingUC1.java
@@ -0,0 +1,10 @@
+package com.bl.Gambling.Simulation;
+
+public class GamblingUC1 {
+	public void Gambler() {
+		   int stake = 100;
+		   int bet = 1;
+		   System.out.println("Gambler put stake of $"+stake+" everyday.");
+		   System.out.println("Gambler bet $"+bet+" on everygame.");
+	   }
+}
